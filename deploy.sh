bebaea15f521cb48e9d378d47d658b641c9a5727
PH Tools <phtools@bldgtyp.com>
2022-09-11 19:00:44 +0200

    dix(deploy): Update dev-requirements

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 echo "Building distribution"
 python setup.py sdist bdist_wheel
 echo "Pushing new version to PyPi"
-twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD
+echo "twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD"
