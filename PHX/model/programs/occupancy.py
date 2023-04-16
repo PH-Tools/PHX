8a88ebe3c123a73f45c8ba45b96a02e3777bf92f
PH Tools <phtools@bldgtyp.com>
2023-04-16 17:47:44 +0200

    fix(occupancy): Fix to default fields

--- PHX/model/programs/occupancy.py
+++ PHX/model/programs/occupancy.py
@@ -4,8 +4,7 @@
 """PHX Program Class for organizing Occupancy Data (People)"""
 
 from __future__ import annotations
-from typing import ClassVar, Any
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from PHX.model.loads.occupancy import PhxLoadOccupancy
 from PHX.model.schedules.occupancy import PhxScheduleOccupancy
@@ -16,5 +15,5 @@ class PhxProgramOccupancy:
     """A PHX Program for the Occupancy (People) with a load and schedule."""
 
     display_name: str = "Unnamed_Occupancy_Program"
-    load: PhxLoadOccupancy = PhxLoadOccupancy()
-    schedule: PhxScheduleOccupancy = PhxScheduleOccupancy()
+    load: PhxLoadOccupancy = field(default_factory=PhxLoadOccupancy)
+    schedule: PhxScheduleOccupancy = field(default_factory=PhxScheduleOccupancy)
